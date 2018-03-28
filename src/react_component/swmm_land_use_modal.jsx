cd2e7a94ca6bcadb6b0951e36b491f4422a24cd7
Paul Chen <paul.chen03@sap.com>
2018-03-28 12:53:01 +0800

    show General/Buildup/Washoff tabs in SwmmLandUseModal dialog

--- src/react_component/swmm_land_use_modal.jsx
+++ src/react_component/swmm_land_use_modal.jsx
@@ -5,7 +5,14 @@ import SwmmModal from "./utility/swmm_modal.jsx";
 const SwmmLandUseModal = props => {
   const {landUse} = props;
   return (
-    <SwmmModal {...props} title="Land Use" width={300}>
+    <SwmmModal {...props} title="Land Use" width={350}>
+      <div className="tabs is-centered is-boxed">
+        <ul>
+          <li className="is-active"><a><span>General</span></a></li>
+          <li><a><span>Buildup</span></a></li>
+          <li><a><span>WashOff</span></a></li>
+        </ul>
+      </div>
       {
         landUse && 
         <table className="table is-hoverable is-bordered" id="swmm-property-list">
