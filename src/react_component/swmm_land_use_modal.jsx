db0a144f7e1070c1a033fd8d6637975564e834b6
Paul Chen <superchen14@gmail.com>
2018-04-05 12:56:37 +0800

    add logic to enable user to switch tab in SwmmLandUseModal

--- src/react_component/swmm_land_use_modal.jsx
+++ src/react_component/swmm_land_use_modal.jsx
@@ -2,38 +2,50 @@ import React from "react";
 import PropTypes from 'prop-types';
 import SwmmModal from "./utility/swmm_modal.jsx";
 
-const SwmmLandUseModal = props => {
-  const {landUse} = props;
-  return (
-    <SwmmModal {...props} title="Land Use" width={350}>
-      <div className="tabs is-centered is-boxed">
-        <ul>
-          <li className="is-active"><a><span>General</span></a></li>
-          <li><a><span>Buildup</span></a></li>
-          <li><a><span>WashOff</span></a></li>
-        </ul>
-      </div>
-      {
-        landUse && 
-        <table className="table is-hoverable is-bordered" id="swmm-property-list">
-          <thead>
-            <tr><th>Property</th><th>Value</th></tr>
-          </thead>
-          <tbody>
-            <tr><th className="normal-col">Name</th><th className="normal-col">{landUse.name}</th></tr>
-          </tbody>
-          <thead>
-            <tr><th>STREET SWEEPING</th><th></th></tr>
-          </thead>
-          <tbody>
-            <tr><th className="normal-col">Interval</th><th className="normal-col">{landUse.interval}</th></tr>
-            <tr><th className="normal-col">Availability</th><th className="normal-col">{landUse.availability}</th></tr>
-            <tr><th className="normal-col">Last Swept</th><th className="normal-col">{landUse.lastSwept}</th></tr>
-          </tbody>
-        </table>
-      }
-    </SwmmModal>
-  );
+const GENERAL_TAB = 0;
+const BUILDUP_TAB = 1;
+const WASSOFF_TAB = 2;
+class SwmmLandUseModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {activeTab: GENERAL_TAB};
+  }
+
+  render() {
+    const {landUse} = this.props;
+    const {activeTab} = this.state;
+    const setActiveTab = activeTab => () => this.setState({activeTab});
+    return (
+      <SwmmModal {...this.props} title="Land Use" width={350} height={460}>
+        <div className="tabs is-centered is-boxed">
+          <ul>
+            <li className={activeTab === GENERAL_TAB ? "is-active" : ""} onClick={setActiveTab(GENERAL_TAB)}><a><span>General</span></a></li>
+            <li className={activeTab === BUILDUP_TAB ? "is-active" : ""} onClick={setActiveTab(BUILDUP_TAB)}><a><span>Buildup</span></a></li>
+            <li className={activeTab === WASSOFF_TAB ? "is-active" : ""} onClick={setActiveTab(WASSOFF_TAB)}><a><span>Washoff</span></a></li>
+          </ul>
+        </div>
+        {
+          landUse && activeTab === GENERAL_TAB &&
+          <table className="table is-hoverable is-bordered" id="swmm-property-list">
+            <thead>
+              <tr><th>Property</th><th>Value</th></tr>
+            </thead>
+            <tbody>
+              <tr><th className="normal-col">Name</th><th className="normal-col">{landUse.name}</th></tr>
+            </tbody>
+            <thead>
+              <tr><th>STREET SWEEPING</th><th></th></tr>
+            </thead>
+            <tbody>
+              <tr><th className="normal-col">Interval</th><th className="normal-col">{landUse.interval}</th></tr>
+              <tr><th className="normal-col">Availability</th><th className="normal-col">{landUse.availability}</th></tr>
+              <tr><th className="normal-col">Last Swept</th><th className="normal-col">{landUse.lastSwept}</th></tr>
+            </tbody>
+          </table>
+        }
+      </SwmmModal>
+    );
+  }
 };
 
 SwmmLandUseModal.propTypes = {
--- src/react_component/utility/swmm_modal.jsx
+++ src/react_component/utility/swmm_modal.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-const SwmmModal = ({title, isActive, onClose, width, children}) => {
+const SwmmModal = ({title, isActive, onClose, width, height, children}) => {
   const className = isActive ? "modal is-active" : "modal";
   width = width ? width : 640;
+  let style = {width};
+  debugger;
+  height && (style["height"] = height);
   return (
     <div className={className}>
       <div className="modal-background"></div>
-      <div className="modal-card" style={{width: width}}>
+      <div className="modal-card" style={style}>
         <header className="modal-card-head">
           <p className="modal-card-title">{title}</p>
           <button className="delete" aria-label="close" onClick={onClose}></button>
@@ -28,6 +31,7 @@ SwmmModal.propTypes = {
   onClose: PropTypes.func.isRequired,
   children: PropTypes.element,
   width: PropTypes.number,
+  height: PropTypes.number
 }
 
 export default SwmmModal;
