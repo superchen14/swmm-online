e2f051c327a15f68bc164e013d57019c70cf9586
Paul Chen <paul.chen03@sap.com>
2018-06-14 15:41:09 +0800

    add a very simple general cypress test

---
+++ cypress/integration/general.js
@@ -0,0 +1,15 @@
+describe("General Tests", function() {
+  it('Visit swmm-online.github.io', function() {
+    cy.visit("https://swmm-online.github.io");
+    assert.isNotNull(cy.get("#swmm-online"));
+    assert.isNotNull(cy.get("#swmm-app"));
+    assert.isNotNull(cy.get("#swmm-header"));
+    assert.isNotNull(cy.get("#swmm-body"));
+    assert.isNotNull(cy.get("#swmm-body #left-pane"));
+    assert.isNotNull(cy.get("#swmm-body #left-pane #left-pane-treeview"));
+    assert.isNotNull(cy.get("#swmm-body #left-pane #left-pane-search"));
+    assert.isNotNull(cy.get("#swmm-body #left-pane #left-pane-list"));
+    assert.isNotNull(cy.get("#swmm-body #main-pane"));
+    assert.isNotNull(cy.get("#swmm-body #right-pane"));
+  })
+});
