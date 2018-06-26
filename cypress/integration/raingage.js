0cd9d83aa49d0afd6bbb76536067bcce4d974ef1
Paul Chen <paul.chen03@sap.com>
2018-06-26 15:46:44 +0800

    add test case to check behavior of click on RainGage item in list pane

--- cypress/integration/raingage.js
+++ cypress/integration/raingage.js
@@ -5,4 +5,20 @@ describe("RainGage Tests", function() {
     rainGageTreeNode.should("have.class", "is-active");
     cy.get("#left-pane-list>.panel>a").should("not.empty");
   });
+
+  it("Click RainGage item in list view should hight and show properties in properties pane", function() {
+    cy.get("ul.menu-list>li>ul>a").contains("Rain Gages").click();
+    const rainGageItem = cy.get("#RAINGAGE-RG1");
+    rainGageItem.click();
+    rainGageItem.should("have.class", "is-active");
+
+    let hash = {};
+    cy.get("#swmm-property-list>tbody>tr").each($tr => {
+      hash[$tr.children(".property-col").html()] = $tr.children(".value-col").html();
+    }).then(() => {
+      assert.equal("RG1", hash.Name);
+      assert.equal("10084.21", hash.X);
+      assert.equal("8210.53", hash.Y);
+    });
+  });
 });
